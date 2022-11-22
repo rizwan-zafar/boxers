2328578457132e602c598743e69f3306fdca1360
SYSTEMS\rizwan.zafar <rizwan.zafar@systemsltd.com>
2022-11-22 20:44:01 +0500

    Attribute Section Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar"
 import Header from "./Components/Header"
+import Attribute from "./Components/Attribute"
 function App() {
   return (
     <div className="App">
    <Navbar />
    <Header />
+   <Attribute />
    <p>asdfasdfds asdfsadf asdfsadf asdfsdfsa asdf</p>
    <p>asdfasdfds asdfsadf asdfsadf asdfsdfsa asdf</p>
    <p>asdfasdfds asdfsadf asdfsadf asdfsdfsa asdf</p>
